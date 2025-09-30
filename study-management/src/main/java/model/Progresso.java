2822b247facb87946a7fc92bae56deba8971cb23
Arth1Droid <barbosarthur7@gmail.com>
2025-09-30 18:25:06 -0300

    Adiciona classe Progresso

---
+++ study-management/src/main/java/model/Progresso.java
@@ -0,0 +1,53 @@
+package model;
+
+public class Progresso {
+	private Long id;
+	private TarefaEstudo tarefa;
+	private int horasEstudadas;
+	private String comentario;
+
+	public Progresso(TarefaEstudo tarefa, int horasEstudadas, String comentario) {
+		this.tarefa = tarefa;
+		this.horasEstudadas = horasEstudadas;
+		this.comentario = comentario;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public TarefaEstudo getTarefa() {
+		return tarefa;
+	}
+
+	public void setTarefa(TarefaEstudo tarefa) {
+		this.tarefa = tarefa;
+	}
+
+	public int getHorasEstudadas() {
+		return horasEstudadas;
+	}
+
+	public void setHorasEstudadas(int horasEstudadas) {
+		this.horasEstudadas = horasEstudadas;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+
+	@Override
+	public String toString() {
+		return "ID: " + id + "Tarefa: " + tarefa.getTitulo() + "Horas Estudadas: " + horasEstudadas + "Comentário: "
+				+ comentario;
+	}
+
+}
